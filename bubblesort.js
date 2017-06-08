e8960a73697b356e47c758a41b17580d9c0d191b
Jagnoor Grewal <noor.grewal@berkeley.edu>
2017-06-09 00:42:15 +0530

    bubblesort WORKING

--- bubblesort.js
+++ bubblesort.js
@@ -1,24 +1,41 @@
-function bubbleSort(arr){
-    var pointer=0;
-    var swapCounter=0;
-    var swapper = 1;
-    if (!arr.length) return [];
-   while (swapper>0) {
-   for (i=0;i<arr.length-1;i++){
-   	     swapper=0;
-        if (arr[i] > arr[i+1]){
-        	pointer=arr[i+1] ;
-        	arr[i+1] =arr[i];
-        	arr[i] = pointer;
-        	swapCounter++;
-        	swapper++;
-		} 
-   }
- }
-   return arr;
+//function bubbleSort(arr){
+//    var pointer=0;
+//    var swapCounter=0;
+//    var swapper = 1;
+//    if (!arr.length) return [];
+//   while (swapper>0) {
+//   for (i=0;i<arr.length-1;i++){
+//   	     swapper=0;
+//        if (arr[i] > arr[i+1]){
+//        	pointer=arr[i+1] ;
+//        	arr[i+1] =arr[i];
+//        	arr[i] = pointer;
+//        	swapCounter++;
+//        	swapper++;
+//		} 
+//   }
+// }
+//   return arr;
+//}
+
+function bubbleSort(arr) {
+	var swapper = 1;
+	if (!arr.length) return [];
+	while(swapper > 0) {
+		swapper = 0;
+		for (i = 0; i <arr.length; i++) {
+			if(arr[i] > arr[i+1]) {
+				swapper++;
+				arr = swap(i, i+1, arr);
+			}
+		}
+	}
+	return arr;
 }
 
-//function swap(a,b) {/
-//    if (b>a) return (a,);
-//    	else  return a;
-//}
+function swap (index1, index2, arr) {
+	var pointer = arr[index2];
+	arr[index2] = arr[index1];
+	arr[index1] = pointer;
+	return arr;
+}
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -7,6 +7,10 @@ describe('Bubble Sort', function(){
   it ('handles an array with multiple elements', function() {
 	  expect(bubbleSort([20, 15, 18, 90, 45])).toEqual([15, 18, 20, 45, 90]);
   })
+  it ('handles an array with multiple elements', function() {
+	  expect(bubbleSort([90, 45, 18, 15, 20])).toEqual([15, 18, 20, 45, 90]);
+  }) 
+	
   // all elements of an array should be a number (use alert like keyword...) should return should be only number 
   // the array should be flattened ... 
   // spy on the sort method 
